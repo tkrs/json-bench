18897f14ef88273d7c9446eaa13bef162f6b7efc
Takeru Sato <type.in.type@gmail.com>
2018-07-02 10:12:24 +0900

    Update dependencies: circe, jacson-scala, and jsoniter

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -7,7 +7,7 @@ object Dependencies {
 
   lazy val argonaut = "io.argonaut" %% "argonaut" % "6.2.1"
 
-  lazy val jacksonScala = "com.fasterxml.jackson.module" %% "jackson-module-scala" % "2.9.5"
+  lazy val jacksonScala = "com.fasterxml.jackson.module" %% "jackson-module-scala" % "2.9.6"
 
   lazy val uPickle = "com.lihaoyi" %% "upickle" % "0.6.6"
   lazy val uJson = "com.lihaoyi" %% "ujson" % "0.6.6"
@@ -16,10 +16,11 @@ object Dependencies {
   lazy val json4sNative = "org.json4s" %% "json4s-native" % json4sVersion
   lazy val json4sJackson = "org.json4s" %% "json4s-jackson" % json4sVersion
 
-  lazy val jsoniterScalaCore = "com.github.plokhotnyuk.jsoniter-scala" %% "core" % "0.28.0"
-  lazy val jsoniterScalaMacros = "com.github.plokhotnyuk.jsoniter-scala" %% "macros" % "0.28.0" % Provided // required only for compile-time
+  lazy val jsoniterVersion = "0.28.1"
+  lazy val jsoniterScalaCore = "com.github.plokhotnyuk.jsoniter-scala" %% "core" % jsoniterVersion
+  lazy val jsoniterScalaMacros = "com.github.plokhotnyuk.jsoniter-scala" %% "macros" % jsoniterVersion % Provided // required only for compile-time
 
-  lazy val circeVersion = "0.9.3"
+  lazy val circeVersion = "0.10.0-M1"
   lazy val circeCore = "io.circe" %% "circe-core" % circeVersion
   lazy val circeGeneric = "io.circe" %% "circe-generic" % circeVersion
   lazy val circeParser = "io.circe" %% "circe-parser" % circeVersion
